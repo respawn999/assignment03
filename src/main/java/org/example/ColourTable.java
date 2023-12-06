5180a22318070ee7b9a534ecf42ed0d54a32ce82
filip <https://github.com/@respawn999>
2023-12-06 13:41:25 +0000

    Third round of tests completed, checked if adding more RGB values than the list contains will throw an exception, tests passed flawlessly

--- src/main/java/org/example/ColourTable.java
+++ src/main/java/org/example/ColourTable.java
@@ -1,24 +1,48 @@
 package org.example;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+
 
 public class ColourTable {
     private int paletteSize;
     private List<int[]> colourTable;
-
     public ColourTable(int paletteSize) {
-        if (invalidPaletteSize(paletteSize)) {
-            throw new IllegalArgumentException("Palette size invalid.");
+        if (!invalidPaletteSize(paletteSize)) {
+            throw new IllegalArgumentException("Invalid specified palette sized, must be a power of 2 and between 1 and 1025");
         }
 
         this.paletteSize = paletteSize;
         this.colourTable = new ArrayList<>();
     }
 
-
     public static boolean invalidPaletteSize(int size) {
         return size > 1 && size < 1025 && (size & (size - 1)) == 0;
     }
+
+
+    public void add(int[] rgbValues) {
+        if (colourTable.size() >= paletteSize) {
+            throw new RuntimeException("Palette size exceeded");
+        }
+        if (!containsRGB(rgbValues)) {
+            colourTable.add(rgbValues); // Add colour to palette
+        }
+
+    }
+
+
+    public boolean containsRGB(int[] rgbValues) {
+        for (int[] colour : colourTable) {
+            if (Arrays.equals(colour, rgbValues)) {
+                return true; // Colour in palette already
+            }
+        }
+        return false; // Colour can be added to palette
+    }
+
 
 }
--- src/test/java/ColourTableTests.java
+++ src/test/java/ColourTableTests.java
@@ -1,12 +1,13 @@
 import org.example.ColourTable;
+
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class ColourTableTests {
    @Test
-   public void validPaletteSize(){
+   public void validPaletteSize() {
       assertTrue(ColourTable.invalidPaletteSize(2));
       assertTrue(ColourTable.invalidPaletteSize(4));
       assertTrue(ColourTable.invalidPaletteSize(8));
@@ -15,5 +16,16 @@ public class ColourTableTests {
       assertFalse(ColourTable.invalidPaletteSize(5));
       assertFalse(ColourTable.invalidPaletteSize(643));
       assertFalse(ColourTable.invalidPaletteSize(3000));
+   }
+
+   @Test
+   public void paletteSizeLimit() {
+      ColourTable colourTable = new ColourTable(4);
+      colourTable.add(new int[]{0, 0, 0});
+      colourTable.add(new int[]{43, 0, 76});
+      colourTable.add(new int[]{33, 0, 11});
+      colourTable.add(new int[]{23, 0, 4});
+
+      RuntimeException exception = assertThrows(RuntimeException.class, () -> colourTable.add(new int[]{0, 0, 255}));
    }
 }
